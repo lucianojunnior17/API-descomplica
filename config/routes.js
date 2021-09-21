953ff3da653c93d7302407521ae64a9ad7253820
Luciano Junior <engluciano.junior@gmail.com>
2021-09-21 03:06:20 -0300

    Routes

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,40 @@
 const express = require('express');
 const routes = express.Router()
 
+
+let db = [
+    { '1' : { Nome: 'Client 1', Idade: '20' }},
+    { '2' : { Nome: 'Client 2', Idade: '22' }},
+    { '3' : { Nome: 'Client 3', Idade: '23' }},
+    { '4' : { Nome: 'Client 1', Idade: '24' }},
+
+]
+routes.get('/', (req, res) => {
+     return res.json(db)
+})
+
+routes.post('/add', (req, res) => {
+    const body = req.body
+
+    if (!body)
+        return res.status(400).end()
+
+    db.push(body)
+        return res.json(body)
+
+})
+
+routes.delete('/:id', (req, res) => {
+    const id = req.params.id
+
+    let newDb = db.filter(iten => {
+        if(!iten[id])
+            return iten
+    })
+
+    db = newDb
+
+    return res.send(newDb)
+})
+
 module.exports = routes
--- index.js
+++ index.js
@@ -2,39 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-conts = require('./config/routes');
+const routes = require ('./config/routes');
 
 
 const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cors());
-app.use(routes());
-
-let db = [
-    { '1' : { Nome: 'Client 1', Idade: '20' }},
-    { '2' : { Nome: 'Client 2', Idade: '22' }},
-    { '3' : { Nome: 'Client 3', Idade: '23' }},
-    { '4' : { Nome: 'Client 1', Idade: '24' }},
-
-]
-
-app.get('/', (req, res) => {
-     return res.json(db)
-})
-
-app.post('/add', (req, res) => {
-    const body = req.body
-
-    if (!body)
-        return res.status(400).end()
-
-    db.push(body)
-        return res.json(body)
-
-})
+app.use(routes);
 
 app.listen(3000, () => {
     console.log('Servidos rodando na porta 3000')
